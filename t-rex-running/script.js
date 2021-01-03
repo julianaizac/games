c789e387dafbfb903fd7410e73817e93a0db7c8c
julianaizac <dev.julianaizac@outlook.com>
2021-01-03 15:05:32 -0300

    style: add images colorful and game over

--- t-rex-running/script.js
+++ t-rex-running/script.js
@@ -24,18 +24,18 @@ function jump() {
           position -= 20
           dino.style.bottom = position + 'px';
         }
-      }, 30) // desce em 2s
+      }, 30) 
     } else {
       position += 20
       dino.style.bottom = position + 'px';
     }
-  }, 30) // sobe em 2s
+  }, 30) 
 }
 
 function createCactus() {
   const cactus = document.createElement('div')
   let cactusPosition = 1300
-  let randomTime = Math.random() * 6000
+  let randomTime = Math.random() * (3000 - 100) + 100
 
   cactus.classList.add('cactus')
   cactus.style.left = 1300 + 'px'
@@ -49,9 +49,12 @@ function createCactus() {
     } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
       clearInterval(leftInterval)
       isGameOver = true;
-      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+      document.body.innerHTML = `
+                                  <h1 class="game-over">GAME OVER</h1>
+                                  <p class="restart">Press F5 to restart</p>
+                                `
     } else {
-      cactusPosition -= 10 // velocidade q se move p esquerda
+      cactusPosition -= 10 
       cactus.style.left = cactusPosition + 'px'
     }
   }, 20)
